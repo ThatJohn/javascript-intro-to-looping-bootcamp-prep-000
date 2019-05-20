93696c075611f3b856b7b809e4408273253896d1
John Fajardo <fajardocj@gmail.com>
2019-05-20 19:59:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,28 +1,31 @@
-function forLoop(array){
-  for(var i=0;i<25;i++){
-    if(i===1){
-      array.push( "I am 1 strange loop" );
-    }else{
-      array.push( `I am ${i} strange loops.`);
-    }
-  }
-  return array;
+function forLoop(array) {
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.')
+		} else {
+			array.push(`I am ${i} strange loops.`)
+		}
+	}
+	return array
 }
 
-function whileLoop(n){
-  while(n>0){
-    console.log(n--)
-  }
-  return 'done';
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n)
+	}
+	return 'done'
 }
 
-function doWhileLoop(array){
-  function maybeTrue(){
-    return Math.random()>=0.5
-  }
+function doWhileLoop(array) {
+	var i = 0
 
-  do{
-   array.pop();
-  }while(maybeTrue(array.length>0 && maybeTrue()));
-  return array;
-  }
+	function incrementVariable() {
+		i = i + 1
+	}
+	do {
+		console.log('array.length = ' + array.length + ' and i = ' + i)
+		array = array.slice(1)
+		incrementVariable()
+	} while (array.length > 0 && i < 5)
+	return array
+} 
